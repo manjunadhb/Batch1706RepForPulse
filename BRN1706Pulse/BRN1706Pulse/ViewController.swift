3ff50a5e7d4e9f3450cb6eca83cd7a1bb86cbf80
manjunadhb <manjunadhb@gmail.com>
2017-11-17 18:02:58 +0530

    i have created a new project and just uploading to global repository

---
+++ BRN1706Pulse/BRN1706Pulse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BRN1706Pulse
+//
+//  Created by Manjunadh Bommisetty on 17/11/17.
+//  Copyright © 2017 BRN. All rights reserved.
+//do something here
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
